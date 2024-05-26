29085f0277dafc2f566fcb1ca11a098dbf618e61
xtombrock <xtombrock1989@gmail.com>
2024-05-26 21:58:16 +0100

    save

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,21 +1,42 @@
 // src/pages/Home.tsx
 import React from 'react';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/react';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+} from '@ionic/react';
+// Logo
+import CatAppLogo from '../../assets/images/logos/CAT APP-01.svg';
 
 const HomePage: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Home</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="p-4">
-        <h1 className="text-4xl font-bold text-red-500 text-center my-8">Welcome to Ionic with Tailwind CSS</h1>
-        <IonButton expand="full" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Click Me
-        </IonButton>
-      </IonContent>
+      <main className='grid bg-yellow-200'>
+        <section className='grid bg-green-300'>
+          <img
+            src={CatAppLogo}
+            alt='Cat App logo'
+            className='w-[150px] h-[150px]'
+          />
+        </section>
+
+        <section className='grid bg-pink-400'>
+            <section className='bg-blue-500 grid gap-4 px-2'>
+              <div>
+                <button className='px-2 py-1 rounded-xl w-full bg-orange-400 text-white text-xl'>COTD</button>
+              </div>
+              <div>
+                <button className='px-2 py-1 rounded-xl w-full bg-orange-400 text-white text-xl'>Endless Cats</button>
+              </div>
+              <div>
+                <button className='px-2 py-1 rounded-xl w-full bg-orange-400 text-white text-xl'>Therapy Mode</button>
+              </div>
+            </section>
+        </section>
+      </main>
     </IonPage>
   );
 };
