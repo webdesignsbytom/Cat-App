b6d9108ffc7eb227f127d1a6789c3a072b2f3f6f
xtombrock <xtombrock1989@gmail.com>
2024-07-07 20:07:38 +0100

    save

--- src/components/account/LoggedInAccountMain.tsx
+++ src/components/account/LoggedInAccountMain.tsx
@@ -16,36 +16,40 @@ const LoggedInAccountMain: React.FC = () => {
   };
 
   return (
-    <div className='grid gap-4'>
-      <article>
-        <h2 className='text-xl font-semibold'>Welcome, {user?.email}</h2>
-        <div>
-          <p>
-            Name: {user?.profile?.firstName} {user?.profile?.lastName}
-          </p>
-          <p>Country: {user?.profile?.country}</p>
-        </div>
-      </article>
+    <div className='grid h-full w-full overflow-hidden'>
+      <div className='grid grid-rows-rev gap-4'>
+        <section>
+          <article>
+            <h2 className='text-xl font-semibold'>Welcome, {user?.email}</h2>
+            <div>
+              <p>
+                Name: {user?.profile?.firstName} {user?.profile?.lastName}
+              </p>
+              <p>Country: {user?.profile?.country}</p>
+            </div>
+          </article>
 
-      <div>
-        <button
-          className='px-2 py-2 rounded-lg w-full h-[48px] bg-main-colour text-white text-2xl font-semibold active:scale-95 active:bg-main-colour-alt shadow-xl'
-          onClick={handleLogout}
-        >
-          Logout
-        </button>
+          <div>
+            <button
+              className='px-2 py-2 rounded-lg w-full h-[48px] bg-main-colour text-white text-2xl font-semibold active:scale-95 active:bg-main-colour-alt shadow-xl'
+              onClick={handleLogout}
+            >
+              Logout
+            </button>
+          </div>
+        </section>
+
+        <section className='mt-10'>
+          <div>
+            <button
+              className='px-2 py-2 rounded-lg w-full h-[48px] bg-main-colour text-white text-2xl font-semibold active:scale-95 active:bg-main-colour-alt shadow-xl'
+              onClick={() => navigateTo('/menu')}
+            >
+              Back
+            </button>
+          </div>
+        </section>
       </div>
-
-      <section className='mt-10'>
-        <div>
-          <button
-            className='px-2 py-2 rounded-lg w-full h-[48px] bg-main-colour text-white text-2xl font-semibold active:scale-95 active:bg-main-colour-alt shadow-xl'
-            onClick={() => navigateTo('/menu')}
-          >
-            Back
-          </button>
-        </div>
-      </section>
     </div>
   );
 };
